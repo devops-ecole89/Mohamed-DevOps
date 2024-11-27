9220543cfbb1e021c641b9592a4d6f1f89be6f26
Septsept <user@Mohamed-Noxia>
2024-11-27 16:41:24 +0100

    Automated commit: All tests passed

--- test/isMohamedTest.py
+++ test/isMohamedTest.py
@@ -1,5 +1,5 @@
 from isMohamed import is_mohamed
 
 def test_is_mohamed():
-    assert is_mohamed('Mohamed') == True
+    assert is_mohamed('Mohamed') == False
     assert is_mohamed('Pierre') == False
